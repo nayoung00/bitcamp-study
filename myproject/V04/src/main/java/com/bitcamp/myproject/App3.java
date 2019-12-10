bd8790094b600b9be36e2f9a4ed4b89fa776d450
nayoung00 <invin121@gmail.com>
2019-12-10 20:45:52 +0900

    프로젝트

---
+++ myproject/V04/src/main/java/com/bitcamp/myproject/App3.java
@@ -0,0 +1,17 @@
+
+package com.bitcamp.myproject;
+
+public class App3 {
+
+
+  public static void main(String[] args) {
+
+    System.out.println("번호: 1");
+    System.out.println("이름: 김나영");
+    System.out.println("이메일: kim@test.com");
+    System.out.println("암호: 1111");
+    System.out.println("사진: kim.png");
+    System.out.println("전화: 1111-2222");
+    System.out.println("가입일: 2019-01-01");
+  }
+}
