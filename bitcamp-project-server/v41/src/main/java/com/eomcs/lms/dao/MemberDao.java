115d5adfbd117f6e42c31364a9ed6d9c469b74b7
nayoung00 <invin1201@gmail.com>
2020-02-27 12:46:18 +0900

    Connection Pool 도입하기

---
+++ bitcamp-project-server/v41/src/main/java/com/eomcs/lms/dao/MemberDao.java
@@ -0,0 +1,21 @@
+package com.eomcs.lms.dao;
+
+import java.util.List;
+import com.eomcs.lms.domain.Member;
+
+public interface MemberDao {
+
+  int insert(Member Member) throws Exception;
+
+  List<Member> findAll() throws Exception;
+
+  Member findByNo(int no) throws Exception;
+
+  int update(Member Member) throws Exception;
+
+  int delete(int no) throws Exception;
+
+  default List<Member> findByKeyword(String keyword) throws Exception {
+    return null;
+  }
+}
