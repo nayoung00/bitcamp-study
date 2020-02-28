1bacf7b0c98ed5d6b47ede7fa3d968c44f29cead
nayoung00 <invin1201@gmail.com>
2020-02-28 17:23:25 +0900

    Mybatis

---
+++ bitcamp-java/src/main/java/com/eomcs/mybatis/ex01/Exam0121.java
@@ -0,0 +1,50 @@
+// Mybatis - 컬럼 이름과 프로퍼티 이름을 일치시키기
+package com.eomcs.mybatis.ex01;
+
+import java.util.List;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Exam0121 {
+
+  public static void main(String[] args) throws Exception {
+    SqlSessionFactory factory = new SqlSessionFactoryBuilder()
+        .build(Resources.getResourceAsStream("com/eomcs/mybatis/ex01/mybatis-config.xml"));
+    SqlSession sqlSession = factory.openSession();
+
+    // SqlSession 객체를 이용하여 SQL 맵퍼 파일에 작성한 SQL 문을 실행한다.
+    // => select 문장
+    // - sqlSession.selectList() : 목록 리턴
+    // - sqlSession.selectOne() : 한 개의 결과 리턴
+    // => insert 문장
+    // - sqlSession.insert()
+    // => update 문장
+    // - sqlSession.update()
+    // => delete 문장
+    // - sqlSession.delete()
+    // => insert/update/delete인 경우 insert()/update()/delete() 메서드 중
+    // 아무거나 호출해도 된다.
+    // 하지만 일관된 유지보수를 위해 메서드를 구분하여 사용하라!
+    //
+    // 메서드 사용법)
+    // => 예) selectList(SQL문 식별자, 파라미터값)
+    // - SQL문 이름 = 그룹명 + "." + SQL문장 아이디
+    // - 그룹명 : <mapper namespace="그룹명"> ... </mapper>
+    // - SQL 문장 아이디 : <select id="SQL문장 아이디">...</select>
+    // - 파라미터 값 = primitive type 및 모든 자바 객체가 가능하다.
+    // 여러 개의 값을 전달할 때는 Map에 담아 넘겨라!
+    List<Board> list = sqlSession.selectList("BoardMapper.selectBoard2");
+
+    for (Board board : list) {
+      System.out.printf("%d, %s, %s, %s, %d\n", board.getNo(), board.getTitle(), board.getContent(),
+          board.getRegisteredDate(), board.getViewCount());
+    }
+
+    sqlSession.close();
+  }
+}
+// 컬럼 이름과 자바 객체의 프로퍼티 이름을 같게 하기
+// => 컬럼의 값을 자바 객체에 담으려면 컬럼과 같은 이름의 프로퍼티가 있어야 한다.
+// => 없다면 위와 같이 프로퍼티 명을 컬럼의 별명으로 지정하라.
