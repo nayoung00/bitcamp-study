2699d559966c7fa6dbf7ce06a1c7611175ae4ce7
nayoung00 <invin1201@gmail.com>
2020-03-25 12:59:45 +0900

    HTTP 프로토콜 적용하기

---
+++ bitcamp-project-server/v54_1/src/main/java/com/eomcs/lms/AppConfig.java
@@ -0,0 +1,19 @@
+package com.eomcs.lms;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.ComponentScan;
+
+// Spring IoC 컨테이너가 탐색할 패키지 설정
+// => 지정한 패키지 및 하위 패키지를 모두 뒤져서
+// @Component 애노테이션이 붙은 클래스를 찾아 객체를 생성한다.
+//
+@ComponentScan(value = "com.eomcs.lms")
+public class AppConfig {
+
+  static Logger logger = LogManager.getLogger(AppConfig.class);
+
+  public AppConfig() {
+    logger.debug("AppConfig 객체 생성!");
+  }
+}
