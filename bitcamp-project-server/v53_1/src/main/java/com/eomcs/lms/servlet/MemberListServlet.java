6099705116be76b581d602d5af507a43bef84c8f
nayoung00 <invin1201@gmail.com>
2020-03-25 10:22:26 +0900

    Log4j 1.2.x 를 사용하여 애플리케이션 로그 처리하기

---
+++ bitcamp-project-server/v53_1/src/main/java/com/eomcs/lms/servlet/MemberListServlet.java
@@ -0,0 +1,29 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Scanner;
+import org.springframework.stereotype.Component;
+import com.eomcs.lms.domain.Member;
+import com.eomcs.lms.service.MemberService;
+import com.eomcs.util.RequestMapping;
+
+@Component
+public class MemberListServlet {
+
+  MemberService memberService;
+
+  public MemberListServlet(MemberService memberService) {
+    this.memberService = memberService;
+  }
+
+  @RequestMapping("/member/list")
+  public void service(Scanner in, PrintStream out) throws Exception {
+    List<Member> members = memberService.list();
+    for (Member member : members) {
+      out.printf("%d, %s, %s, %s, %s, %s, %s\n", member.getNo(), member.getName(),
+          member.getEmail(), member.getPassword(), member.getRegisteredDate(), member.getTel(),
+          member.getPhoto());
+    }
+  }
+}
---
+++ bitcamp-project-server/v53_1/src/main/java/com/eomcs/lms/servlet/MemberSearchServlet.java
@@ -0,0 +1,31 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Scanner;
+import org.springframework.stereotype.Component;
+import com.eomcs.lms.domain.Member;
+import com.eomcs.lms.service.MemberService;
+import com.eomcs.util.Prompt;
+import com.eomcs.util.RequestMapping;
+
+@Component
+public class MemberSearchServlet {
+
+  MemberService memberService;
+
+  public MemberSearchServlet(MemberService memberService) {
+    this.memberService = memberService;
+  }
+
+  @RequestMapping("/member/search")
+  public void service(Scanner in, PrintStream out) throws Exception {
+    String keyword = Prompt.getString(in, out, "검색어?");
+
+    List<Member> members = memberService.search(keyword);
+    for (Member member : members) {
+      out.printf("%d, %s, %s, %s, %s\n", member.getNo(), member.getName(), member.getEmail(),
+          member.getTel(), member.getRegisteredDate());
+    }
+  }
+}
