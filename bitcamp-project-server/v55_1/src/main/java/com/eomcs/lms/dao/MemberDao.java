e097e0006827d91efe0f66e71cba40da244432b6
nayoung00 <invin1201@gmail.com>
2020-04-01 11:55:26 +0900

    JavaEE 의 Servlet 기술 도입하기

---
+++ bitcamp-project-server/v55_1/src/main/java/com/eomcs/lms/dao/MemberDao.java
@@ -0,0 +1,26 @@
+package com.eomcs.lms.dao;
+
+import java.util.List;
+import java.util.Map;
+import com.eomcs.lms.domain.Member;
+
+public interface MemberDao {
+
+  int insert(Member Member) throws Exception;
+
+  List<Member> findAll() throws Exception;
+
+  Member findByNo(int no) throws Exception;
+
+  default Member findByEmailAndPassword(Map<String, Object> params) throws Exception {
+    return null;
+  }
+
+  int update(Member Member) throws Exception;
+
+  int delete(int no) throws Exception;
+
+  default List<Member> findByKeyword(String keyword) throws Exception {
+    return null;
+  }
+}
---
+++ bitcamp-project-server/v55_1/src/main/java/com/eomcs/lms/service/LessonService.java
@@ -0,0 +1,24 @@
+package com.eomcs.lms.service;
+
+import java.util.HashMap;
+import java.util.List;
+import com.eomcs.lms.domain.Lesson;
+
+public interface LessonService {
+
+  Lesson get(int lessonNo) throws Exception;
+
+  int insert(Lesson lesson) throws Exception;
+
+  int delete(int no) throws Exception;
+
+  Lesson findByNo(int no) throws Exception;
+
+  List<Lesson> findAll() throws Exception;
+
+  List<Lesson> search(HashMap<String, Object> params) throws Exception;
+
+  int update(Lesson lesson) throws Exception;
+
+
+}
