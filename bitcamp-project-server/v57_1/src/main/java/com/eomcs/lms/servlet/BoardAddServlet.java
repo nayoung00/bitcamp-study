f33024670a81347937c7cc88311b6e4218c61225
nayoung00 <invin1201@gmail.com>
2020-04-10 17:56:54 +0900

    JSP를 활용하여 서블릿에서 출력 기능을 분리하기

---
+++ bitcamp-project-server/v57_1/src/main/java/com/eomcs/lms/servlet/BoardAddServlet.java
@@ -0,0 +1,53 @@
+package com.eomcs.lms.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.ApplicationContext;
+import com.eomcs.lms.domain.Board;
+import com.eomcs.lms.service.BoardService;
+
+@WebServlet("/board/add")
+public class BoardAddServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    request.getRequestDispatcher("/board/form.jsp").include(request, response);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    try {
+      request.setCharacterEncoding("UTF-8");
+
+      ServletContext servletContext = request.getServletContext(); // this.getServletContext(),
+                                                                   // getServletContext()도 가능
+      ApplicationContext iocContainer =
+          (ApplicationContext) servletContext.getAttribute("iocContainer");
+      BoardService boardService = iocContainer.getBean(BoardService.class);
+
+      Board board = new Board();
+      board.setTitle(request.getParameter("title"));
+
+      boardService.add(board);
+
+      response.sendRedirect("list");
+
+    } catch (Exception e) {
+      request.setAttribute("error", e);
+      request.setAttribute("url", "list");
+      // 포워드 인클루드에서 루트는 현재 웹어플리케이션을 의미한다.
+      request.getRequestDispatcher("/error").forward(request, response);
+
+    }
+  }
+}
---
+++ bitcamp-project-server/v57_1/src/main/webapp/board/form.jsp
@@ -0,0 +1,16 @@
+<%@page import="com.eomcs.lms.domain.Board"%>
+<%@page import="java.util.List"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"
+    trimDirectiveWhitespaces="true"%>
+
+<jsp:include page="/header.jsp"/>
+
+<h1>게시물 입력(JSP)</h1>
+<form action='add' method='post'>
+내용:<br>
+<textarea name='title' rows='5' cols='60'></textarea><br>
+<button>등록</button>
+</form>
+
+<jsp:include page="/footer.jsp"/>
